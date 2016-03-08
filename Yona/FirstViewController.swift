38a98b39b6778d6d74232dd6c758743d7fd2b6e3
Ben Smith <bensmith@static.kpn.net>
2016-03-08 11:19:04 +0100

    Initial Commit

---
+++ Yona/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Yona
+//
+//  Created by Ben Smith on 08/03/16.
+//  Copyright © 2016 Ben Smith. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
